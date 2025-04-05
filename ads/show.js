95adbb4346e8c6ba795f727ce904a7172270468c
chikapiyo <yos.hayashi.icloud@gmail.com>
2025-04-06 08:36:16 +0900

    Update show.js

--- ads/show.js
+++ ads/show.js
@@ -2,7 +2,7 @@
 (function () {
   const adHTML = `
     <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-      <img src="/Image/yoshinobu1991_3D_cartoon-style_photo_of_a_big_golden_Tulip_pl_d06e5105-9b16-4060-9697-fcbda07a1113_0.png?text=サンプル広告" alt="広告バナー">
+      <img src="https://chikapiyo.github.io/ad-test/Image/yoshinobu1991_3D_cartoon-style_photo_of_a_big_golden_Tulip_pl_d06e5105-9b16-4060-9697-fcbda07a1113_0.png?text=サンプル広告" alt="広告バナー">
     </a>
   `;
 
