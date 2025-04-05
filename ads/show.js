67bee859d0caa2c7431b2195e236f8a0dce50a0c
chikapiyo <yos.hayashi.icloud@gmail.com>
2025-04-06 08:30:24 +0900

    Update show.js

--- ads/show.js
+++ ads/show.js
@@ -2,7 +2,7 @@
 (function () {
   const adHTML = `
     <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-      <img src="https://via.placeholder.com/300x250?text=サンプル広告" alt="広告バナー">
+      <img src="https://www.xlsoft.com/jp/products/ispring/suite/index.html?twclid=27fenw4vfi0atmqda5zzw6fnis?text=サンプル広告" alt="広告バナー">
     </a>
   `;
 
